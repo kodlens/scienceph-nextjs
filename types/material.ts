83ab19d732e8ce21c49d18b94b6f94dcbd94a53c
eshen <etiennewayne@gmail.com>
2026-05-04 15:59:41 +0800

    update material

--- types/material.ts
+++ types/material.ts
@@ -46,7 +46,7 @@ export type Material = {
   publish_date: string;
   is_press_release: boolean;
   category_id?: number;
-  category?: Category | string;
+  category?: Category;
   created_at: string ;
   updated_at: string ;
 };
